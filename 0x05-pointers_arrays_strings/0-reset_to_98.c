b777c65ffe46f717e703e3a4efb24401ffc3efaa
mungaijoy <mungaiijoy@gmail.com>
2022-09-20 16:34:57 -0700

    Resetting

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
-#include <main.h>
+#include "main.h"
 
 /**
-* reset_to_98 - a unction to update the reference of a pointer 
+* reset_to_98 -> a unction to update the reference of a pointer 
 * @n: pointer to be updated 
 */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-	*n=98
+	*n=98;
 }
-
